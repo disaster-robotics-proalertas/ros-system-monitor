0691337160af5e52be129a719b1ea0ec9c3e45ea
rgmaidana <renanmaidana@gmail.com>
2019-04-07 01:51:46 +0000

    Changed check for stale module

--- bin/driver_monitor.py
+++ bin/driver_monitor.py
@@ -12,7 +12,7 @@ class Monitor:
         self.diag_pub = rospy.Publisher("/diagnostics", DiagnosticArray, queue_size=10)
         self.drivers = rospy.get_param("/asv_description/%s/sensor_drivers" % self.vehicle_name)
         self.rate = rospy.Rate(1)
-        self.last_update = self.last_update = rospy.Time.now().to_sec()  
+        self.last_update = rospy.Time.now().to_sec()
 
     def run(self):
         nodes = rosnode.get_node_names()
@@ -23,20 +23,23 @@ class Monitor:
             diag_msg.status[0].hardware_id = driver.split('/')[1]
 
             # Check if message is stale (older than 35 seconds)
-            elapsed = rospy.Time().now().to_sec() - self.last_update
+            elapsed = rospy.Time.now().to_sec() - self.last_update
             if elapsed > 35:
                 diag_msg.status[0].level = DiagnosticStatus.STALE
+		diag_msg.status[0].values.clear()
                 diag_msg.status[0].values.insert(0, KeyValue(key = '%s Driver' % driver.split('/')[2], value = 'Stale'))
                 diag_msg.status[0].values.insert(1, KeyValue(key = 'Time Since Update', value = str(elapsed)))
+
+            if driver in nodes:
+                diag_msg.status[0].level = DiagnosticStatus.OK
+		diag_msg.status[0].values.clear()
+                diag_msg.status[0].values.insert(0, KeyValue(key = '%s Driver' % driver.split('/')[2], value = 'Up'))
+                self.last_update = rospy.Time.now().to_sec()
             else:
-                if driver in nodes:
-                    diag_msg.status[0].level = DiagnosticStatus.OK
-                    diag_msg.status[0].values.insert(0, KeyValue(key = '%s Driver' % driver.split('/')[2], value = 'Up'))
-                    self.last_update = rospy.Time.now().to_sec()
-                else:
-                    diag_msg.status[0].level = DiagnosticStatus.ERROR
-                    diag_msg.status[0].values.insert(0, KeyValue(key = '%s Driver' % driver.split('/')[2], value = 'Down'))
-                    self.last_update = rospy.Time.now().to_sec()  
+                diag_msg.status[0].level = DiagnosticStatus.ERROR
+		diag_msg.status[0].values.clear()
+                diag_msg.status[0].values.insert(0, KeyValue(key = '%s Driver' % driver.split('/')[2], value = 'Down'))
+                self.last_update = rospy.Time.now().to_sec()
 
             diag_msg.header.stamp = rospy.Time.now()
             self.diag_pub.publish(diag_msg)
